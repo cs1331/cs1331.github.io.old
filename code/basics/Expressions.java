17b83b043599a4acde61f2c30a810fc4c2d1002a
Chris Simpkins <chris.simpkins@gmail.com>
2017-09-21 16:50:31 -0400

    Update code/basics/Expressions.java with type conversion examples

--- code/basics/Expressions.java
+++ code/basics/Expressions.java
@@ -1,7 +1,15 @@
 public class Expressions {
 
     public static void main(String[] args) {
+        // int division -- answer may be unexpected
         float twoThirds = 2 / 3;
         System.out.println(twoThirds);
+
+        // widening conversions are made implicitly
+        double x = 1;
+
+        // Narrowing conversions must be explicit.
+        // This won't compile without explicit type cast
+        int y = (int) 1.0;
     }
 }
