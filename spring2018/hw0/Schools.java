65b9759016886a0f8df44ae5f24157a7c14efad0
Chris Simpkins <chris.simpkins@gmail.com>
2018-01-09 15:18:15 -0500

    Add spring2018/hw0/Schools.java

---
+++ spring2018/hw0/Schools.java
@@ -0,0 +1,6 @@
+public class Schools {
+    public static void main(String[] args) {
+        String gt = "Georgia Institute of Technology";
+        System.out.printf("%s > %s%n", gt, uga);
+    }
+}
