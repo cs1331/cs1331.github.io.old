7ae05a765ef9782b3832343324f13c2edff37c01
Chris Simpkins <chris.simpkins@gmail.com>
2018-02-16 08:42:31 -0500

    Update interface examples

--- code/oop/Named.java
+++ code/oop/Named.java
@@ -1,7 +1,4 @@
 public interface Named {
 
-    // This is a default method. It's
-    default public String getName() {
-        return "Robert Paulson";
-    }
+    String getName();
 }
---
+++ code/oop/Person.java
@@ -0,0 +1,6 @@
+public class Person implements Named {
+
+    public String getName() {
+        return "Robert Paulson";
+    }
+}
