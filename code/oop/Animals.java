5fe5f821be02515df36d0e53ba79e1b3390074fc
Chris Simpkins <chris.simpkins@gmail.com>
2018-02-28 09:04:26 -0500

    Update code/oop/Animals.java to make easier to use with jshell

--- code/oop/Animals.java
+++ code/oop/Animals.java
@@ -1,40 +1,76 @@
+interface Named {
+    String getName();
+}
+
+abstract class Animal implements Named {
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void speak();
+}
+
+class Mammal extends Animal {
+
+    public Mammal(String name) {
+        super(name);
+    }
+
+    public void speak() {
+        System.out.println("Hello!");
+    }
+}
+
+class Dog extends Mammal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void speak() {
+        System.out.println("Woof, woof!");
+    }
+    public void wagTail() {
+        System.out.println("(wags tail)");
+    }
+}
+
+class Cat extends Mammal {
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    public void speak() {
+        System.out.println("Meow!");
+    }
+}
+
+// Notice that this is the only public class in this file. What if we
+// made other classes public?
 public class Animals {
 
-    public static abstract class Animal {
-        public abstract void speak();
-    }
-
-    public static class Mammal extends Animal {
-        public void speak() {
-            System.out.println("Hello!");
-        }
-    }
-    public static class Dog extends Mammal {
-        public void speak() {
-            System.out.println("Woof, woof!");
-        }
-        public void wagTail() {
-            System.out.println("(wags tail)");
-        }
-    }
-    public static class Cat extends Mammal {
-        public void speak() {
-            System.out.println("Meow!");
-        }
-    }
-
     public static void main(String[] args) {
-        Mammal fido = new Dog();
+        // Won't compile because it's abstract
+        // Animal a = new Animal();
+        Mammal fido = new Dog("Chloe");
         fido.speak();
-        // Dog fido2 = fido; // Won't compile: Java won't implicitly downcast
+        Dog fido2 = (Dog) fido; // Won't compile: Java won't implicitly downcast
         ((Mammal) fido).speak();
 
-        Mammal mittens = (Mammal) new Cat(); // Safe
-        Mammal sparky = new Mammal();
+        Mammal mittens = (Mammal) new Cat("Fiona"); // Safe
+        Mammal sparky = new Mammal("Bailey");
         // Compiles, but will cause a ClassCastException at run-time,
         Dog huh = (Dog) sparky;
-        // so we won't even get here.
-        huh.wagTail();
+        // // so we won't even get here.
+        // huh.wagTail();
     }
 
 }
