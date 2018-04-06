c04a81799c67223aa4a3bab61a00e0bb27323e90
Chris Simpkins <chris.simpkins@gmail.com>
2018-04-06 09:07:22 -0400

    Add streams slides and example code

--- code/collections/super-troopers/StreamTroopers.java
+++ code/collections/super-troopers/StreamTroopers.java
@@ -1,8 +1,17 @@
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamTroopers {
+
+    static class HasMustache implements Predicate<Trooper> {
+
+        public boolean test(Trooper t) {
+            return t.hasMustache();
+        }
+    }
+
 
     public static void main(String[] args) {
         List<Trooper> troopers = new ArrayList<>();
@@ -16,14 +25,36 @@ public class StreamTroopers {
 
         System.out.println("Before sorting:\n" + troopers);
 
-        long mustaches =
+        // Mustache counting ...
+
+        // using named class:
+        long mc1 =
+            troopers.stream().filter(new HasMustache()).count();
+
+        System.out.println("Mustaches: " + mc1);
+
+        // using anonymous inner class:
+        long mc2 =
+            troopers.stream().filter(new Predicate<Trooper>() {
+                    public boolean test(Trooper t) {
+                        return t.hasMustache();
+                    }
+                }).count();
+
+        System.out.println("Mustaches: " + mc2);
+
+        // using lambda expression:
+        long mc3 =
+            troopers.stream().filter(t -> t.hasMustache()).count();
+
+        System.out.println("Mustaches: " + mc3);
+
+        // using method reference:
+        long mc4 =
             troopers.stream().filter(Trooper::hasMustache).count();
 
-        long nonMustaches =
-            troopers.stream().filter(e -> !e.hasMustache()).count();
+        System.out.println("Mustaches: " + mc4);
 
-        System.out.println("Mustaches: " + mustaches);
-        System.out.println("Non-Mustaches: " + nonMustaches);
 
         Comparator<Trooper> byMustacheThenName =
             Comparator.comparing(Trooper::hasMustache)
@@ -45,5 +76,4 @@ public class StreamTroopers {
             .collect(Collectors.toList());
         System.out.println("Squares: " + squares);
     }
-
 }
